7906c7ad7d147479d07534e46ed7529cee1bf12e
Md Amanullah <muhammadamanullah6001@gmail.ccom>
2024-09-23 19:53:16 +0600

    everything done

--- js/noakhali.js
+++ js/noakhali.js
@@ -21,7 +21,7 @@ document
       historyItem.className = "border-2 p-5 rounded-md my-2";
       historyItem.innerHTML = `
         <p>${donation} Taka is Donated for Donate for Flood at Noakhali, Bangladesh</p>
-        <p>Date: ${new Date()}</p>
+        <p class="font-light text-gray-600">Date: ${new Date()}</p>
         `;
 
       historySection.insertBefore(historyItem, historySection.firstChild);
